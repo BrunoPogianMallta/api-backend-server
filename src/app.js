49b02ca14e30a209a98a0302debb4674c705929f
Mallta <brunopmalltaads@gmail.com>
2025-05-28 00:58:57 -0300

    add:pre flight request

--- src/app.js
+++ src/app.js
@@ -1,15 +1,27 @@
 const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
 const app = express();
-require('dotenv').config(); 
+
+const allowedOrigins = process.env.CORS_ORIGINS.split(',');
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  optionsSuccessStatus: 200 
+};
+
+app.use(cors(corsOptions));
+
+
+app.options('*', cors(corsOptions));
 
 app.use(express.json());
 
-const authRoutes = require('./routes/auth.routes');
-app.use('/api/auth', authRoutes);
-
-
-app.use((req, res) => {
-  res.status(404).json({ message: 'Rota não encontrada' });
-});
-
-module.exports = app;
