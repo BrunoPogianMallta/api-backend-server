5097212ce8dd737c35465570e68ba46da6ad8014
Mallta <brunopmalltaads@gmail.com>
2025-05-28 15:59:23 -0300

    fix

--- src/services/account.service.js
+++ src/services/account.service.js
@@ -1,36 +1,25 @@
-const jwt = require('jsonwebtoken');
-const { hashPassword } = require('../utils/password');
-const { generateSalt, generateVerifier } = require('../utils/srp');
-const accountService = require('./account.service');
+// services/account.service.js
+const pool = require('../config/db');
 
-async function register(username, password, email) {
-  const normalizedUsername = username.toUpperCase();
-
-  const sha1Pass = hashPassword(normalizedUsername, password);
-  const salt = generateSalt();
-  const verifier = generateVerifier(normalizedUsername, password, salt);
-
-  await accountService.createAccount({
-    username: normalizedUsername,
-    shaPassHash: sha1Pass,
-    email,
-    verifier,
-    salt
-  });
+async function createAccount({ username, shaPassHash, email, verifier, salt }) {
+  const sql = `
+    INSERT INTO accounts (username, sha_pass_hash, email, v, s)
+    VALUES (?, ?, ?, ?, ?)
+  `;
+  const [result] = await pool.execute(sql, [username, shaPassHash, email, verifier, salt]);
+  return result;
 }
 
-async function login(username, password) {
-  const normalizedUsername = username.toUpperCase();
-  const hashed = hashPassword(normalizedUsername, password);
-
-  const user = await accountService.getAccountByCredentials(normalizedUsername, hashed);
-  if (!user) throw new Error('Credenciais inválidas');
-
-  const token = jwt.sign({ id: user.id, username: user.username }, process.env.JWT_SECRET, {
-    expiresIn: '1h'
-  });
-
-  return { user, token };
+async function getAccountByCredentials(username, shaPassHash) {
+  const sql = `
+    SELECT id, username, email FROM accounts
+    WHERE username = ? AND sha_pass_hash = ?
+  `;
+  const [rows] = await pool.execute(sql, [username, shaPassHash]);
+  return rows[0];
 }
 
-module.exports = { register, login };
+module.exports = {
+  createAccount,
+  getAccountByCredentials
+};
