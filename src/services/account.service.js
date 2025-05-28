e285227f1f707164e033256f38a346f2d6325318
Mallta <brunopmalltaads@gmail.com>
2025-05-28 16:41:55 -0300

    fix

--- src/services/account.service.js
+++ src/services/account.service.js
@@ -3,7 +3,7 @@ const pool = require('../config/db');
 
 async function createAccount({ username, shaPassHash, email, verifier, salt }) {
   const sql = `
-    INSERT INTO accounts (username, sha_pass_hash, email, v, s)
+    INSERT INTO account (username, sha_pass_hash, email, v, s)
     VALUES (?, ?, ?, ?, ?)
   `;
   const [result] = await pool.execute(sql, [username, shaPassHash, email, verifier, salt]);
@@ -12,7 +12,7 @@ async function createAccount({ username, shaPassHash, email, verifier, salt }) {
 
 async function getAccountByCredentials(username, shaPassHash) {
   const sql = `
-    SELECT id, username, email FROM accounts
+    SELECT id, username, email FROM account
     WHERE username = ? AND sha_pass_hash = ?
   `;
   const [rows] = await pool.execute(sql, [username, shaPassHash]);
