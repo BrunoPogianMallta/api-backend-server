5091dc7fa8a67d93ef0fc0dfcb045d9c474c2531
Mallta <brunopmalltaads@gmail.com>
2025-05-29 01:45:27 -0300

    fix

--- src/validators/auth.validator.js
+++ src/validators/auth.validator.js
@@ -4,12 +4,14 @@ const registerSchema = Joi.object({
   username: Joi.string().alphanum().min(3).max(15).required(),
   password: Joi.string().min(6).required(),
   email: Joi.string().email().required()
-  
+
 });
 
 const loginSchema = Joi.object({
   username: Joi.string().required(),
-  password: Joi.string().required()
+  password: Joi.string().required(),
+  remember: Joi.boolean().optional()
+
 });
 
 module.exports = {
